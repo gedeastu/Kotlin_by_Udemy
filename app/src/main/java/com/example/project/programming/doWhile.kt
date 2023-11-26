ab073160e20d006245ee847ed93e65c478184a83
gedeastu <gede.astu@gmail.com>
2023-11-26 22:59:17 +0700

    up

---
+++ app/src/main/java/com/example/project/programming/doWhile.kt
@@ -0,0 +1,10 @@
+package com.example.project.programming
+
+fun main(args: Array<String>) {
+    var speed:Int = 1000
+
+    do {
+        println(speed)
+        speed++
+    }while (speed < 1200)
+}
