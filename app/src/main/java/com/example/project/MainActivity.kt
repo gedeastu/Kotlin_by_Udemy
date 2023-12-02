b49d96c882b99252ccfd5bc6385766da36ec9a12
gedeastu <gede.astu@gmail.com>
2023-12-02 11:18:02 +0700

    up

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -6,10 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,9 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.project.ui.theme.ProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +41,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Layouts("Android")
+                    Button()
                 }
             }
         }
@@ -40,10 +50,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Layouts(name: String, modifier: Modifier = Modifier) {
     Surface(modifier = Modifier) {
         Box(modifier = Modifier){
-            Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+            Row(modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceAround) {
                 Box(modifier = Modifier
                     .height(100.dp)
                     .width(100.dp)
@@ -52,11 +62,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     Text(
                         text = "Hello $name!",
                         modifier = Modifier
+                            .background(colorResource(id = R.color.red))
+                            .padding(10.dp)
                         //.align(Alignment.Center)
                         ,
-                        color = colorResource(id = R.color.red,)
+                        color = colorResource(id = R.color.white,),
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
                     )
-                    Text(text = stringResource(id = R.string.app_name))
+                    //Text(text = stringResource(id = R.string.app_name))
                 }
                 Box(modifier = Modifier
                     .height(100.dp)
@@ -68,14 +83,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Header(){
-
+fun Button(){
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.size(200.dp,70.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.purple_700)),
+            shape = RoundedCornerShape(10.dp)
+            )
+        {
+            Text(text = "Do your Magic 😱", fontSize = 15.sp)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ProjectTheme {
-        Greeting("Android")
+        //Layouts("Android")
+        Button()
     }
 }
